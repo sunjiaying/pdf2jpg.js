7a1a12c4dbcf99e8280ac7189b252f61e441c69b
sunjiaying <sunjoin@qq.com>
2019-07-01 10:40:06 +0800

    添加分辨率参数

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const exec = require('child_process').exec;
 
-function convert(pdfFile, jpgFile, callback) {
+function convert(pdfFile, jpgFile, resolutionRatio, callback) {
     fs.stat(pdfFile, function(err, stat) {
         if(err == null) {
-            exec('gs -sDEVICE=jpeg -dFirstPage=1 -dLastPage=1 -o ' + jpgFile + ' ' + pdfFile,
+            exec('gs -r' + resolutionRatio + ' -sDEVICE=jpeg -dFirstPage=1 -dLastPage=1 -o ' + jpgFile + ' ' + pdfFile,
                 function (err, stdout, stderr) {
                     if (err) {
                         callback(err);
